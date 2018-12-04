ddacd9f4f8af9570e6d6e3c6e802c4979e00a99a
Ryan Watkins <rdwatkin@outlook.com>
2018-12-04 15:14:28 -0800

    added some helper functions

--- functions.pl
+++ functions.pl
@@ -13,3 +13,34 @@ haversine_radians( Lat1, Lon1, Lat2, Lon2, Distance ) :-
    Dist is 2 * atan2( sqrt( A ), sqrt( 1 - A )),
    Distance is Dist * 3961.
 
+
+% -- Degree-Minutes-Seconds to Radians
+dmsToRads( degmin( Deg, Min ), RetVal) :-
+	RetVal is ((Deg + Min/60) * pi) / 180.
+
+
+% -- NOT in prolog
+not( X ) :- X, !, fail.
+not( _ ).
+
+% -- add two times
+sumTimes( time(h1, m1), time(h2, m2), time(retHr, retMin) ) :-
+	retHr is h1+h2,
+	retMin is m1+m2,
+	% !(min > 60)
+	retHr is retHr + floor(retMin / 60),
+	retMin is mod(retMin, 60).
+
+% -- given airport Get airport Latitude and Longitude in radians
+getAirportLocation(Airport1, retLat, retLon) :-
+	airport(Airport1, _, degmin(a1, o1), degmin(a2, o2)),
+	dmsToRads(a1, o1, retLat),
+	dmsToRads(a2, o2, retLon).
+
+% -- returns length of flight between two airports in hours
+flightTime( A1, A2, retVal) :-
+	getAirportLocation(A1, Lat1, Long1),
+	getAirportLocation(A2, Lat2, Long2),
+	haversine_radians( Lat1, Long1, Lat2, Long2, Distance),
+	retVal is Distance / 500.
+
