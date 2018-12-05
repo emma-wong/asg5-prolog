b4be8f2221409e6a945799e1a7d24af3d0381e59
Ryan Watkins <rdwatkin@outlook.com>
2018-12-04 21:15:19 -0800

    converted hours to time(hr,min) & implemented modified listpath

--- functions.pl
+++ functions.pl
@@ -11,7 +11,7 @@ haversine_radians( Lat1, Lon1, Lat2, Lon2, Distance ) :-
    A is sin( Dlat / 2 ) ** 2
       + cos( Lat1 ) * cos( Lat2 ) * sin( Dlon / 2 ) ** 2,
    Dist is 2 * atan2( sqrt( A ), sqrt( 1 - A )),
-   Distance is Dist * 3961.
+   Distance is Dist * 3959.
 
 
 % -- Degree-Minutes-Seconds to Radians
@@ -37,10 +37,46 @@ getAirportLocation(Airport1, retLat, retLon) :-
 	dmsToRads(a1, o1, retLat),
 	dmsToRads(a2, o2, retLon).
 
-% -- returns length of flight between two airports in hours
-flightTime( A1, A2, retVal) :-
+% -- returns length of flight between two airports
+flightTime( A1, A2, time(RetHours, RetMinutes)) :-
 	getAirportLocation(A1, Lat1, Long1),
 	getAirportLocation(A2, Lat2, Long2),
 	haversine_radians( Lat1, Long1, Lat2, Long2, Distance),
-	retVal is Distance / 500.
+	TotalTime is Distance / 500,
+	RetHours is floor(TotalTime),
+	RetMinutes is floor((TotalTime-RetHours)*60).
+	
+
+% -- Given a flight return the arrival time
+flightArrivalTime( flight(Depart, Arrive, time(Hr, Min)), ArrivalTime) :-
+	flightTime(Depart, Arrive, FT),
+	sumTimes( time(Hr, Min), FT, ArrivalTime).
+
+% -- Given two times Check if t2 is atleast 30minutes after t1
+validDelayTimes( time(H1, M1), time(H2, M2)) :-
+	Minutes1 is M1 + H1 * 60,
+	Minutes2 is M2 + H2 * 60,
+	Minutes1 + 29 < Minutes2.
+
+% -- There are no overnight trips
+checkForOvernight( flight(Depart, Arrive, DepTime) ) :-
+	flightArrivalTime( flight(Depart, Arrive, DepTime), flight(Hr, Min) ),
+	Hr < 24.
+
+% -- ListPath, a modified version of provided code
+ListPath(Node, End, Outlist) :-
+	ListPath(Node, End, time(0,0), [Node], Outlist).
+
+ListPath(Node, Node, _,_,_).
+
+ListPath(Node, End, Time, Tried, [flight(Node, Next, Dep)|List]) :-
+	flight(Node, Next, Dep),
+	validDelayTimes(Time, Dep),
+	flightArrivalTime( flight(Node, Next, Dep), ArrivalTime),
+	checkForOvernight( flight(Node, Next, Dep)),
+	not( member(Next, Tried)),
+	ListPath(Next, End, ArrivalTime, [Next|Tried], List). 
+	
+	
+
 
